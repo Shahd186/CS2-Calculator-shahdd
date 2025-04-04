dcac6e197bf505e6651672fbca476e8a379e48bb
shahd <shahdelfeky@aucegypt.edu>
2025-04-04 03:22:31 +0200

    Change the input numbers for add and subtruct functions

--- test.cpp
+++ test.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main() {
-    srand(time(0)); // Seed for random number generation
+    srand(time(0)); 
 
-    cout << "Addition (5 + 3): " << add(5, 3) << endl;
-    cout << "Subtraction (5 - 3): " << subtract(5, 3) << endl;
+    cout << "Addition (10 +10 ): " << add(10 ,10 ) << endl;
+    cout << "Subtraction (10 - 10): " << subtract(10 , 10) << endl;
     cout << "Multiplication (5 * 3): " << multiply(5, 3) << endl;
     cout << "Division (10 / 2): " << divide(10, 2) << endl;
     cout << "Factorial (5!): " << factorial(5) << endl;
