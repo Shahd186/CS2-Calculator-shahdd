92280e2148da0700585d85176fccd3dce729bb9f
shahd <shahdelfeky@aucegypt.edu>
2025-04-04 03:09:22 +0200

    Second commit: Establish calculator library

---
+++ build/CMakeFiles/test_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_calculator.dir/calculator.cpp.obj"
+  "CMakeFiles/test_calculator.dir/calculator.cpp.obj.d"
+  "CMakeFiles/test_calculator.dir/test.cpp.obj"
+  "CMakeFiles/test_calculator.dir/test.cpp.obj.d"
+  "libtest_calculator.dll.a"
+  "test_calculator.exe"
+  "test_calculator.exe.manifest"
+  "test_calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
